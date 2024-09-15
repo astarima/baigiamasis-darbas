72f1c52b9db0b4a403f2ff26c3945c93279f5f1b
astarima <astarima@yahoo.com>
2024-09-15 16:20:56 +0300

    baigiamasis

---
+++ javascript.js
@@ -0,0 +1,98 @@
+const text1 = document.getElementById('gėlės1');
+const text2 = document.getElementById('gėlės2');
+const text3 = document.getElementById('gėlės3');
+const text4 = document.getElementById('gėlės4');
+const text5 = document.getElementById('gėlės5');
+const text6 = document.getElementById('gėlės6');
+const text7 = document.getElementById('gėlės7');
+const text8 = document.getElementById('gėlės8');
+
+    text1.addEventListener('mouseover', function() {
+            text1.style.color = 'green';
+        
+        });
+        text1.addEventListener('mouseout', function() {
+            text1.style.color = 'black';
+        });
+
+        document.getElementById("gėlės1").addEventListener("click", function() {
+            window.location.href = "ruta.html";
+        });
+
+    text2.addEventListener('mouseover', function() {
+            text2.style.color = 'green';
+        
+        });
+        text2.addEventListener('mouseout', function() {
+            text2.style.color = 'black';
+        });
+
+        document.getElementById("gėlės2").addEventListener("click", function() {
+            window.location.href = "pinavija.html";
+        });
+    text3.addEventListener('mouseover', function() {
+            text3.style.color = 'green';
+        
+        });
+        text3.addEventListener('mouseout', function() {
+            text3.style.color = 'black';
+        });
+
+        document.getElementById("gėlės3").addEventListener("click", function() {
+            window.location.href = "ramune.html";
+        });
+    text4.addEventListener('mouseover', function() {
+            text4.style.color = 'green';
+        
+        });
+        text4.addEventListener('mouseout', function() {
+            text4.style.color = 'black';
+        });
+
+        document.getElementById("gėlės4").addEventListener("click", function() {
+            window.location.href = "linas.html";
+        });
+    text5.addEventListener('mouseover', function() {
+            text5.style.color = 'green';
+        
+        });
+        text5.addEventListener('mouseout', function() {
+            text5.style.color = 'black';
+        });
+
+        document.getElementById("gėlės5").addEventListener("click", function() {
+            window.location.href = "aguona.html";
+        });
+    text6.addEventListener('mouseover', function() {
+            text6.style.color = 'green';
+        
+        });
+        text6.addEventListener('mouseout', function() {
+            text6.style.color = 'black';
+        });
+
+        document.getElementById("gėlės6").addEventListener("click", function() {
+            window.location.href = "kosmeja.html";
+        });
+    text7.addEventListener('mouseover', function() {
+            text7.style.color = 'green';
+        
+        });
+        text7.addEventListener('mouseout', function() {
+            text7.style.color = 'black';
+        });
+
+        document.getElementById("gėlės7").addEventListener("click", function() {
+            window.location.href = "jurginas.html";
+        });
+    text8.addEventListener('mouseover', function() {
+            text1.style.color = 'green';
+        
+        });
+        text8.addEventListener('mouseout', function() {
+            text8.style.color = 'black';
+        });
+
+        document.getElementById("gėlės8").addEventListener("click", function() {
+            window.location.href = "gvazdikelis.html";
+        });
