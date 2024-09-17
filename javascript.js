3bddb3c513d3a4cbff6d74772e77a076ff60ef98
astarima <astarima@yahoo.com>
2024-09-17 09:44:44 +0300

    Initial commit

--- javascript.js
+++ javascript.js
@@ -1,11 +1,26 @@
 const text1 = document.getElementById('gėlės1');
+const img1 = document.getElementById('img1');
+
 const text2 = document.getElementById('gėlės2');
+const img2 = document.getElementById('img2');
+
 const text3 = document.getElementById('gėlės3');
+const img3 = document.getElementById('img3');
+
 const text4 = document.getElementById('gėlės4');
+const img4 = document.getElementById('img4');
+
 const text5 = document.getElementById('gėlės5');
+const img5 = document.getElementById('img5');
+
 const text6 = document.getElementById('gėlės6');
+const img6 = document.getElementById('img6');
+
 const text7 = document.getElementById('gėlės7');
+const img7 = document.getElementById('img7');
+
 const text8 = document.getElementById('gėlės8');
+const img8 = document.getElementById('img8');
 
     text1.addEventListener('mouseover', function() {
             text1.style.color = 'green';
@@ -16,6 +31,10 @@ const text8 = document.getElementById('gėlės8');
         });
 
         document.getElementById("gėlės1").addEventListener("click", function() {
+            window.location.href = "ruta.html";
+        });
+
+        document.getElementById("img1").addEventListener("click", function() {
             window.location.href = "ruta.html";
         });
 
@@ -30,6 +49,10 @@ const text8 = document.getElementById('gėlės8');
         document.getElementById("gėlės2").addEventListener("click", function() {
             window.location.href = "pinavija.html";
         });
+        document.getElementById("img2").addEventListener("click", function() {
+            window.location.href = "pinavija.html";
+        });
+
     text3.addEventListener('mouseover', function() {
             text3.style.color = 'green';
         
@@ -41,6 +64,11 @@ const text8 = document.getElementById('gėlės8');
         document.getElementById("gėlės3").addEventListener("click", function() {
             window.location.href = "ramune.html";
         });
+
+        document.getElementById("img3").addEventListener("click", function() {
+            window.location.href = "ramune.html";
+        });
+
     text4.addEventListener('mouseover', function() {
             text4.style.color = 'green';
         
@@ -52,6 +80,11 @@ const text8 = document.getElementById('gėlės8');
         document.getElementById("gėlės4").addEventListener("click", function() {
             window.location.href = "linas.html";
         });
+
+        document.getElementById("img4").addEventListener("click", function() {
+            window.location.href = "linas.html";
+        });
+
     text5.addEventListener('mouseover', function() {
             text5.style.color = 'green';
         
@@ -63,6 +96,11 @@ const text8 = document.getElementById('gėlės8');
         document.getElementById("gėlės5").addEventListener("click", function() {
             window.location.href = "aguona.html";
         });
+
+        document.getElementById("img5").addEventListener("click", function() {
+            window.location.href = "aguona.html";
+        });
+
     text6.addEventListener('mouseover', function() {
             text6.style.color = 'green';
         
@@ -74,6 +112,11 @@ const text8 = document.getElementById('gėlės8');
         document.getElementById("gėlės6").addEventListener("click", function() {
             window.location.href = "kosmeja.html";
         });
+
+        document.getElementById("img6").addEventListener("click", function() {
+            window.location.href = "kosmeja.html";
+        });
+
     text7.addEventListener('mouseover', function() {
             text7.style.color = 'green';
         
@@ -85,6 +128,11 @@ const text8 = document.getElementById('gėlės8');
         document.getElementById("gėlės7").addEventListener("click", function() {
             window.location.href = "jurginas.html";
         });
+
+        document.getElementById("img7").addEventListener("click", function() {
+            window.location.href = "jurginas.html";
+        });
+
     text8.addEventListener('mouseover', function() {
             text8.style.color = 'green';
         
@@ -94,5 +142,9 @@ const text8 = document.getElementById('gėlės8');
         });
 
         document.getElementById("gėlės8").addEventListener("click", function() {
+            window.location.href = "gvazdikelis.html";
+        });
+
+        document.getElementById("img8").addEventListener("click", function() {
             window.location.href = "gvazdikelis.html";
         });
